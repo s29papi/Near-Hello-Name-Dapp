326a1e4221ee8b9b5834391d0dad61814d2b7f65
Usih Anselm <83395635+usihanselm@users.noreply.github.com>
2022-05-26 15:32:00 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -30,44 +30,44 @@ impl HelloContract  {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
+#[cfg(test)]
+mod tests {
 
-//   use super::*;
-//   use near_sdk::MockedBlockchain;
-//   use near_sdk::{testing_env, VMContext};
-//   fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
-//      VMContext {
-//          current_account_id: "alice.testnet".to_string(),
-//          signer_account_id: "robert.testnet".to_string(),
-//          signer_account_pk: vec![0, 1, 2],
-//          predecessor_account_id: "jane.testnet".to_string(),
-//          input,
-//          block_index: 0,
-//          block_timestamp: 0,
-//          account_balance: 0,
-//          account_locked_balance: 0,
-//          storage_usage: 0,
-//          attached_deposit: 0,
-//          prepaid_gas: 10u64.pow(18),
-//          random_seed: vec![0, 1, 2],
-//          is_view,
-//          output_data_receivers: vec![],
-//          epoch_height: 19,
-//      }
-//  }
+  use super::*;
+  use near_sdk::MockedBlockchain;
+  use near_sdk::{testing_env, VMContext};
+  fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
+     VMContext {
+         current_account_id: "alice.testnet".to_string(),
+         signer_account_id: "robert.testnet".to_string(),
+         signer_account_pk: vec![0, 1, 2],
+         predecessor_account_id: "jane.testnet".to_string(),
+         input,
+         block_index: 0,
+         block_timestamp: 0,
+         account_balance: 0,
+         account_locked_balance: 0,
+         storage_usage: 0,
+         attached_deposit: 0,
+         prepaid_gas: 10u64.pow(18),
+         random_seed: vec![0, 1, 2],
+         is_view,
+         output_data_receivers: vec![],
+         epoch_height: 19,
+     }
+ }
 
-//  // mark individual unit tests with #[test] for them to be registered and fired
-//  #[test]
-//  fn create_value_and_get_value() {
-//      // set up the mock context into the testing environment
-//      let context = get_context(vec![], false);
-//      testing_env!(context);
-//      // instantiate a contract variable with the greeting equal to hello, world!
-//      let mut contract = HelloContract{ greeting: String::from("hello, world!") };
-//      contract.input("Bieber!".to_string());
-//      println!("Greeting after calling create_value(func): {}", contract.output());
-//      // confirm that we are greeting the right person!
-//      assert_eq!("hello, Bieber!", contract.output());
-//  }
-// }
+ // mark individual unit tests with #[test] for them to be registered and fired
+ #[test]
+ fn create_value_and_get_value() {
+     // set up the mock context into the testing environment
+     let context = get_context(vec![], false);
+     testing_env!(context);
+     // instantiate a contract variable with the greeting equal to hello, world!
+     let mut contract = HelloContract{ greeting: String::from("hello, world!") };
+     contract.input("Bieber!".to_string());
+     println!("Greeting after calling create_value(func): {}", contract.output());
+     // confirm that we are greeting the right person!
+     assert_eq!("hello, Bieber!", contract.output());
+ }
+}
